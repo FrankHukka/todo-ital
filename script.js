2d8c3902c3a142a2c916b9102f1cba9695138184
FrankHukka <riinatmaenpaa@gmail.com>
2024-11-08 12:39:25 +0200

    more functionality added

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-const left = document.getElementById("items-left");
-
 const todoCreate = document.querySelector("input");
-const todoList = document.querySelector("table");
+const todoList = document.querySelector(".todo-list");
+const newBtn = document.querySelector(".newb");
+
+function addNewTodo() {
+    const todoRow = document.createElement("tr");
+    todoRow.classList.add("todo-row");
+
+    const todoItem = document.createElement("td");
+    todoItem.classList.add("list-item");
+
+    const checkBox = document.createElement("td");
+    checkBox.append(document.createElement("button"));
+    checkBox.classList.add("listed");
+
+    const removeBox = document.createElement("td");
+    const xB = document.createElement("img");
+    xB.setAttribute("src", "images/icon-cross.svg");
+    removeBox.classList.add("remove");
+    removeBox.append(xB);
+
+    todoList.appendChild(todoRow);
+    todoRow.appendChild(checkBox);
+    todoRow.appendChild(todoItem);
+    todoRow.appendChild(removeBox);
+
+    todoItem.textContent = todoCreate.value;
+    todoCreate.value = "";
+}
 
 todoCreate.addEventListener("keydown", (eve) => {
     if (eve.key === "Enter") {
         if (todoCreate.value === "") {
             eve.preventDefault;
         } else {
-            const todoRow = document.createElement("tr");
-            const todoItem = document.createElement("td");
-            todoList.appendChild(todoRow)
-            .appendChild(todoItem);
-            todoItem.textContent = todoCreate.value;
-            todoCreate.value = "";
+            addNewTodo();
         }
     }
 })
+
+newBtn.addEventListener("click", (ev) => {
+    if (todoCreate.value === "") {
+        ev.preventDefault;
+    } else {
+        addNewTodo();
+    }
+})
+
+const leftBox = document.getElementById("items-left");
+// const itemsCount = todoList.childElementCount;
+// leftBox.textContent = `${itemsCount} items left`;
