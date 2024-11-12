8093cbd0af8e03d2c1fd4ddb9c73c9e6797dc92a
FrankHukka <riinatmaenpaa@gmail.com>
2024-11-12 14:53:03 +0200

    hover states, delete button

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 const todoCreate = document.querySelector("input");
 const todoList = document.querySelector(".todo-list");
+
 const newBtn = document.querySelector(".newb");
 
-function addNewTodo() {
-    const todoRow = document.createElement("tr");
+const leftBox = document.querySelector("output");
+
+const rows = todoList.rows;
+
+const addNewTodo = () => {
+    const todoRow = todoList.insertRow();
     todoRow.classList.add("todo-row");
 
-    const todoItem = document.createElement("td");
-    todoItem.classList.add("list-item");
-
-    const checkBox = document.createElement("td");
+    const checkBox = todoRow.insertCell(0);
     checkBox.append(document.createElement("button"));
     checkBox.classList.add("listed");
 
-    const removeBox = document.createElement("td");
+    const todoItem = todoRow.insertCell(1);
+    todoItem.classList.add("list-item");
+
+    const removeBox = todoRow.insertCell(2);
     const xB = document.createElement("img");
     xB.setAttribute("src", "images/icon-cross.svg");
     removeBox.classList.add("remove");
     removeBox.append(xB);
 
-    todoList.appendChild(todoRow);
-    todoRow.appendChild(checkBox);
-    todoRow.appendChild(todoItem);
-    todoRow.appendChild(removeBox);
+    xB.addEventListener("click", () => {
+        todoList.deleteRow(todoRow);
+        leftBox.textContent = rows.length;
+    });
 
+    leftBox.textContent = rows.length;
     todoItem.textContent = todoCreate.value;
     todoCreate.value = "";
 }
@@ -36,7 +42,7 @@ todoCreate.addEventListener("keydown", (eve) => {
             addNewTodo();
         }
     }
-})
+});
 
 newBtn.addEventListener("click", (ev) => {
     if (todoCreate.value === "") {
@@ -44,8 +50,4 @@ newBtn.addEventListener("click", (ev) => {
     } else {
         addNewTodo();
     }
-})
-
-const leftBox = document.getElementById("items-left");
-// const itemsCount = todoList.childElementCount;
-// leftBox.textContent = `${itemsCount} items left`;
+});
